c5a16d365f92dc040c495c8e1c5a0a1597a75cf6
Max Base <MaxBaseCode@Gmail.Com>
2020-07-15 10:09:31 +0430

    Update and rename ubash.c to ushell.c

--- ushell.c
+++ ushell.c
@@ -1,8 +1,8 @@
 /*
- * @Name: ubash
+ * @Name: ushell
  * @Author: Max Base
  * @Date: 2020-07-15
- * @Repository: https://github.com/BaseMax/ubash
+ * @Repository: https://github.com/BaseMax/ushell
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,12 +10,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void ubash_allocation_error() {
-    fprintf(stderr, "ubash: Unable to allocate buffer\n");
+void ushell_allocation_error() {
+    fprintf(stderr, "ushell: Unable to allocate buffer\n");
     exit(EXIT_FAILURE);
 }
 
-int ubash_run(char **args) {
+int ushell_run(char **args) {
     // https://man7.org/linux/man-pages/man2/waitid.2.html
     // https://stackoverflow.com/questions/47441871/why-should-we-check-wifexited-after-wait-in-order-to-kill-child-processes-in-lin
     // https://www.gnu.org/software/libc/manual/html_node/Process-Completion-Status.html
@@ -24,12 +24,12 @@ int ubash_run(char **args) {
     pid_t pid = fork();
     if(pid == 0) {
         if(execvp(args[0], args) == -1) {
-            perror("ubash");
+            perror("ushell");
         }
         exit(EXIT_FAILURE);
     }
     else if(pid < 0) {
-        perror("ubash");
+        perror("ushell");
     }
     else {
         do {
@@ -40,30 +40,30 @@ int ubash_run(char **args) {
     return 1;
 }
 
-char *ubash_read(void) {
+char *ushell_read(void) {
     char *buffer;
     size_t bufsize = 1024;
     buffer = (char *)malloc(bufsize * sizeof(char));// sizeof(char) is 1
     if(!buffer) {
-        ubash_allocation_error();
+        ushell_allocation_error();
     }
     if(getline(&buffer, &bufsize, stdin) == -1) {
         if(feof(stdin)) {
             exit(EXIT_SUCCESS);
         }
         else {
-            perror("ubash: getline\n");
+            perror("ushell: getline\n");
             exit(EXIT_FAILURE);
         }
     }
     return buffer;
 }
 
-char **ubash_split(char *line) {
+char **ushell_split(char *line) {
     int bufsize = 64, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
     if(!tokens) {
-        ubash_allocation_error();
+        ushell_allocation_error();
     }
     char *token = strtok(line, " \t\r\n\a");
     while(token != NULL) {
@@ -73,7 +73,7 @@ char **ubash_split(char *line) {
             bufsize += 64;
             tokens = realloc(tokens, bufsize * sizeof(char*));
             if(!tokens) {
-                ubash_allocation_error();
+                ushell_allocation_error();
             }
         }
         token = strtok(NULL, " \t\r\n\a");
@@ -88,15 +88,15 @@ int main(int argc, char **argv, char** env) {
     int status;
     do {
         printf("\x1B[32m");//yellow
-        printf("ubash# ");
+        printf("ushell# ");
         printf("\x1B[0m");//normal
-        command = ubash_read();
-        args = ubash_split(command);
+        command = ushell_read();
+        args = ushell_split(command);
         if(strcmp(command, "exit")==0) {
             status=0;
         }
         else if(args[0]!=NULL){
-            status=ubash_run(args);
+            status=ushell_run(args);
         }
         free(command);
         free(args);
